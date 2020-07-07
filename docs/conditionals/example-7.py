14d52330fdff37ae7d3dd32ea0c0fb831e0fabd3
Barnsa <barnsa@uni.coventry.ac.uk>
2020-07-07 10:44:30 +0100

    Mostly completed conditionals section. TODO: reorder and make it fit in with everything else.

---
+++ docs/conditionals/example-7.py
@@ -0,0 +1,19 @@
+# example-7.py
+# code to illustrate complex if statement evaluations
+variable_1 = 17 
+variable_2 = 17 
+variable_3 = 8
+variable_4 = True 
+variable_5 = False
+
+if (variable_1 > variable_2 or variable_4) and (variable_1 == variable_2) \
+    or not variable_4: 
+        print("evaluation 1 is true!")
+else:
+    print("evaluation 1 is False!")
+        
+if (variable_1 > variable_2 or variable_4) and (variable_1 == variable_2) \
+    and not variable_5: 
+        print("evaluation 2 is true!")
+else:
+    print("evaluation 2 is False!")
